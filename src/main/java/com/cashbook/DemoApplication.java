c4b3fcf74d10cb02ed0552f1e03a3d4b7d26235b
longtao <longtaotao1314@gmail.com>
2019-05-19 18:11:23 +0800

    mabytis 生成example

--- src/main/java/com/cashbook/DemoApplication.java
+++ src/main/java/com/cashbook/DemoApplication.java
@@ -1,9 +1,11 @@
 package com.cashbook;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.cashbook.dao")
 public class DemoApplication {
 
     public static void main(String[] args) {
